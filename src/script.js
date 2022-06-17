38763d6d20b325f726270fa407694045b0c52776
joannahAU <101956154+joannahAU@users.noreply.github.com>
2022-06-17 13:28:04 +0800

    Added temperature C-F converter

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
 function displayCurrentWeather(response) {
   console.log(response.data);
+  celsiusTemperature = response.data.main.temp;
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  document.querySelector("#temperature").innerHTML =
+    Math.round(celsiusTemperature);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
@@ -17,42 +17,62 @@ function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayCurrentWeather);
 }
-
-function searchLocation(position) {
-  let apiKey = "e73d0ba114f999fe7d2a6332ed2e2979";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayCurrentWeather);
-}
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   searchCity(city);
 }
 
-searchCity("New York");
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  fahrenheitLink.classList.remove("inactive");
+  celsiusLink.classList.add("inactive");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  temperatureElement.innerHTML = Math.round((celsiusTemperature * 9) / 5 + 32);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  celsiusLink.classList.remove("inactive");
+  fahrenheitLink.classList.add("inactive");
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+}
+
+let celsiusTemperature = null;
 
 let searchForm = document.querySelector("#search-form");
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 searchForm.addEventListener("submit", handleSubmit);
+
+searchCity("New York");
+
+//function searchLocation(position) {
+//let apiKey = "e73d0ba114f999fe7d2a6332ed2e2979";
+//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+//axios.get(apiUrl).then(displayCurrentWeather);
+//}
+//function getCurrentLocation(event) {
+//event.preventDefault();
+//navigator.geolocation.getCurrentPosition(searchLocation);
+//}
 
 // let currentLocationButton = document.querySelector("#current-location-button");
 // currentLocationButton.addEventListener("click", getCurrentLocation);
-//Challenge 3
-//function convertToFahrenheit(event) {
-// event.preventDefault();
-// let temperatureElement = document.querySelector("#temperature");
-// let temperature = temperatureElement.innerHTML;
-// temperature = Number(temperature);
-// temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
-// }
 
-// let fahrenheitLink = document.querySelector("#fahrenheit-link");
-// fahrenheitLink.addEventListener("click", convertToFahrenheit);
+//Challenge 3
 
 let currentTime = new Date();
 let dateElement = document.querySelector("#date");
